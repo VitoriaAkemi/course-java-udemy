9a34897d91995532748a95a7b0dc850a9b31346f
Vitoria Akemi <rm88077@fiap.com.br>
2021-12-23 17:54:12 -0300

    testando na classe program

--- Sessao12-PedidosDeComercio/src/aplication/Program.java
+++ Sessao12-PedidosDeComercio/src/aplication/Program.java
@@ -13,6 +13,14 @@ public class Program {
 
 		
 		System.out.println(order);
+		
+		OrderStatus os1 = OrderStatus.DELIVERED;
+		
+		// convertendo valor string para enum
+		OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
+		
+		System.out.println(os1);
+		System.out.println(os2);
 	}
 
 }
