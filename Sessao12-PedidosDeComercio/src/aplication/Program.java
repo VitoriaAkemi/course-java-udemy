738526e589521ddbf11563a59a1a4f54dcdb09d0
Vitoria Akemi <rm88077@fiap.com.br>
2021-12-23 17:29:06 -0300

    Classe Order Com get,set e construtor

--- Sessao12-PedidosDeComercio/src/aplication/Program.java
+++ Sessao12-PedidosDeComercio/src/aplication/Program.java
@@ -1,10 +1,18 @@
 package aplication;
 
+
+import java.util.Date;
+import entites.Order;
+import entites.enums.OrderStatus;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
 
+		
+		System.out.println(order);
 	}
 
 }
