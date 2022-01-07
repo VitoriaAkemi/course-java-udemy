5fbfa9adcd0c6c87940edae621675136e5d9f564
Vitoria Akemi <rm88077@fiap.com.br>
2022-01-07 19:44:46 -0300

    criacao dos atributos e metodos get/set e construtor

--- Sessao14-Fixacao_Polimorfismo/src/entites/ImportedProduct.java
+++ Sessao14-Fixacao_Polimorfismo/src/entites/ImportedProduct.java
@@ -1,5 +1,26 @@
 package entites;
 
-public class ImportedProduct {
+public class ImportedProduct extends Product{
+		
+	private Double customFee;
+	
+	public ImportedProduct() {
+	}
 
+	public ImportedProduct(String name, Double price, Double customFee) {
+		super(name, price);
+		this.customFee = customFee;
+	}
+
+	public Double getCustomFee() {
+		return customFee;
+	}
+
+	public void setCustomFee(Double customFee) {
+		this.customFee = customFee;
+	}
+
+	
+	
+	
 }
